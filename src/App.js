74dafb1ac3935f79d1b9b660067254ad25746da8
hidwoc <heidischoi@gmail.com>
2021-06-29 13:25:13 -0400

    - import Route to App.js - Route skeleton

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-
+import { Route } from "react-router-dom"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Route exact path="/">
+        <h2>Summary</h2>
+      </Route>
+      <Route path="/details/:id">
+        <h2>Details</h2>
+      </Route>
     </div>
   );
 }
