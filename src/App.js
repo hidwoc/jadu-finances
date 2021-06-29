742a1b50ac539caf7cedd4d91b802d45bbfd4034
hidwoc <heidischoi@gmail.com>
2021-06-29 13:46:21 -0400

    - successful API GET request for expensesData and salesData

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
+import { useEffect, useState } from "react"
 import { Route } from "react-router-dom";
+import axios from "axios";
 import Nav from "./components/Nav";
-import './App.css';
+import { baseURLSales, baseURLExpenses, config } from "./services";
+import "./App.css";
 
 function App() {
+  const [expensesData, setExpensesData] = useState([]);
+  const [salesData, setSalesData] = useState([])
+
+  useEffect(() => {
+    const fetchExpensesData = async() => {
+      const resp = await axios.get(baseURLExpenses, config);
+      setExpensesData(resp.data.records);
+    }
+    fetchExpensesData();
+
+    const fetchSalesData = async() => {
+      const resp = await axios.get(baseURLSales, config);
+      setSalesData(resp.data.records);
+    }
+    fetchSalesData();
+  },[])
+
+
   return (
     <div className="App">
       <Nav />
       <Route exact path="/">
         <h2>Summary</h2>
       </Route>
-       {/**
-        * ? Leave this as NOT an exact path so I can gray it out in CSS?
-        *  */}
+      {/**
+       * ? Leave this as NOT an exact path so I can gray it out in CSS?
+       *  */}
       <Route path="/details/:id">
         <h2>Details</h2>
       </Route>
       {/**
-        * TODO: Link in ToForm.jsx
-        *  */}
+       * TODO: Link in ToForm.jsx
+       *  */}
       <Route path="/details/:id/form">
         <h2>Form</h2>
       </Route>
