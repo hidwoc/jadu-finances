fa40f8ebf0e4bdbd7ec5fa4e714bd8c1bce72b2c
hidwoc <heidischoi@gmail.com>
2021-07-02 11:15:01 -0400

    create divs for all App elements

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Route } from "react-router-dom";
 import axios from "axios";
 import BatchMenu from "./components/BatchMenu";
 import Details from "./components/Details";
-import Form from "./components/Form"
+import Form from "./components/Form";
 import Nav from "./components/Nav";
 import Summary from "./components/Summary";
 import { baseURLSales, baseURLExpenses, config } from "./services";
@@ -19,7 +19,7 @@ function App() {
   const [selectedSales, setSelectedSales] = useState([]);
   const [selectedExpenses, setSelectedExpenses] = useState([]);
   const [toggleFetch, setToggleFetch] = useState(false);
-  
+
   // get unique Keys from BatchMenu
   // const [keys, setKeys] = useState([]);
   // let totalsObject = {};
@@ -36,9 +36,8 @@ function App() {
       setSalesData(resp.data.records);
     };
     fetchSalesData();
-    
   }, [toggleFetch]);
-  
+
   /** 
    // separate useEffect so .filter has a chance to be called on newly fetched data???
    * TODO:  update, rawData, selectedData, calculations on POST/PUT/DELETE*/
@@ -50,35 +49,41 @@ function App() {
     setSelectedExpenses(
       expensesData.filter((expense) => expense.fields.batch === selectedBatch)
     );
-
-  }, [selectedBatch, salesData, expensesData])
-
+  }, [selectedBatch, salesData, expensesData]);
 
   return (
     <div className="App">
       <header>
         <h3>JADU FINANCES</h3>
       </header>
-      <Nav />
-      <BatchMenu
-        salesData={salesData}
-        expensesData={expensesData}
-        setSelectedBatch={setSelectedBatch}
-        // setKeys={setKeys}
-      />
-
-      <Route exact path="/">
-        <Summary /> {/*  totalsObject={totalsObject} */}
-      </Route>
-      {/**
-       * ? Leave this as NOT an exact path so I can gray it out in CSS?
-       *  */}
-      <Route path="/details/:id">
-        <Details selectedExpenses={selectedExpenses} selectedSales={selectedSales}/>
-      </Route>
-      <Route path="/details/:id/form">
-        <Form setToggleFetch={setToggleFetch} />
-      </Route>
+      <div id="nav-div">
+        <Nav />
+      </div>
+      <div id="batchmenu-div">
+        <BatchMenu
+          salesData={salesData}
+          expensesData={expensesData}
+          setSelectedBatch={setSelectedBatch}
+          // setKeys={setKeys}
+        />
+      </div>
+      <div id="main-div">
+        <Route exact path="/">
+          <Summary /> {/*  totalsObject={totalsObject} */}
+        </Route>
+        {/**
+         * ? Leave this as NOT an exact path so I can gray it out in CSS?
+         *  */}
+        <Route path="/details/:id">
+          <Details
+            selectedExpenses={selectedExpenses}
+            selectedSales={selectedSales}
+          />
+        </Route>
+        <Route path="/details/:id/form">
+          <Form setToggleFetch={setToggleFetch} />
+        </Route>
+      </div>
     </div>
   );
 }
